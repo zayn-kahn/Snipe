5322f8fdf1d9eda426920a2f6e04594a1dedc5b8
burraq101325 <76736607+burraq101325@users.noreply.github.com>
2022-04-26 21:06:44 +0500

    url-update

--- snipe/addL.js
+++ snipe/addL.js
@@ -7,7 +7,7 @@ const addresses = {// Necessary Address
   WETH: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",//WETH 0xc778417E063141139Fce010982780140Aa0cD5Ab
   R_router: "0xD99D1c33F9fC3444f8101754aBC46c52416550D1",//Router 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
   factory: "0x6725F303b657a9451d8BA641348b6761A6CC7a17",//Facory 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
-  token: '0xe7c6d08f02c264a96bb02313ac374f55c9a40e1d'
+  token: '0xE7C6D08F02c264a96bB02313AC374F55C9A40E1d'
 }
 
 //Necessary Interfaces
@@ -35,17 +35,14 @@ const R_router = new ethers.Contract( //R_router Contract Instance
         ); 
         abiDecoder.addABI(required_Items.routerABI)
 const erc = new ethers.Contract(
-    addresses.token,
+    addresses.WETH,
     required_Items.ERC20,
     account
 )
 
 
 var init = async function () {
-    // return
-    // console.time("Approve")
-    // await erc.approve(addresses.R_router, ethers.utils.parseUnits('1000000', 'ether')); 
-    // console.timeEnd("Approve\n")
+    
     console.time("AddL")
     addLiquidity = await R_router.addLiquidity(
         addresses.WETH,
--- snipe/mempoolScan.js
+++ snipe/mempoolScan.js
@@ -1,35 +1,23 @@
 var ethers = require("ethers");
-// var Tx = require('ethereumjs-tx').Transaction;
 const abiDecoder = require('./abi-decoder');
 const required_Items = require('./cfg.json');
 
-var customWsProvider = new ethers.providers.WebSocketProvider(required_Items.wss_bsc);
-// var filter = { 
-//   fromBlock: "pending",
-//   toBlock: "latest",
-//   address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D" //uniswap Router
-// };
+abiDecoder.addABI(required_Items.routerABI);
+var customWsProvider = new ethers.providers.WebSocketProvider("wss://ftm.getblock.io/mainnet/?api_key=512aed0a-647a-49e5-8a9e-b6d432a653ee");
 var init = function () {
   customWsProvider.on("pending", (tx) => {
     customWsProvider.getTransaction(tx).then(function (transaction) {
       if (!transaction){
         return}
         else{
-        abiDecoder.addABI(required_Items.routerABI)
-        Data = abiDecoder.decodeMethod(transaction.data);
-        if (!Data) { // && Data.name === 'addLiquidity|| Data.name' === 'swapETHForExactTokens'
-            return}
-          else {
-            console.log("Transaction",transaction)
-            console.log("Decoded:", transaction.data,
-             `
-            Func:`, Data, `
-            Token0:`, token0 = Data.params[0].value,`
-            Token1`, toeken1 = Data.params[1].value,`
-            GasPrice:`, Gas = transaction.gasPrice.toString()-1,
-            (transaction.gasPrice - 1).toString(), transaction.gasPrice.toString()
-            );}
-          }
+          console.log("Transaction")
+          Decode = abiDecoder.decodeMethod(transaction.data)
+          if (Decode !==undefined){
+          console.log(transaction)
+          console.log(Decode)
+        }
+        return 
+      }
     });
   });
 };
